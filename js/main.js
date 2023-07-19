b2b40067e016f903f6dcf652e5ff5df94676c4c4
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2023-07-19 10:41:12 +0500

    prepare lawyer section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-
+$(document).ready(function(){
+  $(".lawyer__list").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
