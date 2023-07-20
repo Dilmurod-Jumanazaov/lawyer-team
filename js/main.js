057ad49866a451785e6e9567b999333d1cf1e366
Khurmatbek <xurmatbekolloyorov1514@gmail.com>
2023-07-21 01:57:52 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -3,23 +3,22 @@ $(document).ready(function(){
         dots: true,
         infinite: false,
         speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 2,
+        slidesToScroll: 1,
         responsive: [
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              
             }
           },
           {
             breakpoint: 600,
             settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
+              slidesToShow: 1,
+              slidesToScroll: 1
             }
           },
           {
